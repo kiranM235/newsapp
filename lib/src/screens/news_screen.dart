82a501941024a9cc272e30117fe6c59515ac84e8
KiranMaharjan23 <kiranmaharjan894@outlook.com>
2021-11-04 07:02:19 +0545

    get dependencies

--- lib/src/screens/news_screen.dart
+++ lib/src/screens/news_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:news/src/bloc/news/news_bloc.dart';
 import 'package:news/src/bloc/news_bloc.dart';
 import 'package:news/src/repo/repository.dart';
 
